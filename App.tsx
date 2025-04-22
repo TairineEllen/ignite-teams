a9f0a653fe175c92dd7c8f4ceda9cc28c85a304a
TairineEllen <tairine.ellen@gmail.com>
2025-04-22 15:30:22 -0300

    add theme and fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import {Groups} from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import theme from './src/theme';
+import { Groups } from '@screens/Groups';
+
 
 export default function App() {
+  // fonte disponível para toda aplicação
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
 
