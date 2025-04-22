cd68f833a741fe24e189ddc1a5f31f02c5b0b60e
TairineEllen <tairine.ellen@gmail.com>
2025-04-22 17:47:36 -0300

    add GroupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,6 +2,7 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highligth } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 export function Groups() {
   return (
@@ -11,6 +12,8 @@ export function Groups() {
         title='Turmas'
         subtitle='Jogue com sua turma'
       />
+
+      <GroupCard title="Teste"/>
       
     </Container >
   );
