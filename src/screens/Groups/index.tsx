73ced4e04d7338c9dff7dcfefb391279eb6cbd9f
TairineEllen <tairine.ellen@gmail.com>
2025-04-22 17:22:58 -0300

    add title and subtitle component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,16 @@
 
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Highligth } from '@components/Highlight';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highligth
+        title='Turmas'
+        subtitle='Jogue com sua turma'
+      />
       
     </Container >
   );
