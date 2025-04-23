2e12acb5b8fd7d9ee3167e82957ccc4bc7ae2c00
TairineEllen <tairine.ellen@gmail.com>
2025-04-23 10:52:29 -0300

    add Input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content,Icon } from "./styles";
 import { Highligth } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup () {
     return (
@@ -15,8 +16,14 @@ export function NewGroup () {
                     subtitle="Crie uma turma para adicionar pessoas"
                 />
 
-                <Button title="Criar"/>
+                <Input
+                    placeholder="Nome da turma"
+                />
 
+                <Button
+                    title="Criar"
+                    style={{ marginTop: 20}}
+                />
             </Content>
         </Container>
     )
