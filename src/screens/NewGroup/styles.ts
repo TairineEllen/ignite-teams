6cd29d4bb73c186754963c26b6e762221e7df2c7
TairineEllen <tairine.ellen@gmail.com>
2025-04-23 11:08:29 -0300

    refactor with css helper

--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -5,13 +5,11 @@ export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     padding: 24px;
-
 `
 
 export const Content = styled.View`
     flex: 1;
     justify-content: center;
-
 `
 
 export const Icon = styled(UsersThree).attrs(({ theme }) => ({
