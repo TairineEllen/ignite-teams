de4c13610e6e19bdbc6ec5a7273f369c683fbe4c
TairineEllen <tairine.ellen@gmail.com>
2025-04-24 13:57:08 -0300

    add navigation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Container, Content,Icon } from "./styles";
 import { Highligth } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup () {
+    const navigation = useNavigation();
+
+    function handleNew() {
+        navigation.navigate("players", { group: "Bichos"})
+    }
+
     return (
         <Container>
             <Header showBackButton/>
@@ -23,6 +30,7 @@ export function NewGroup () {
                 <Button
                     title="Criar"
                     style={{ marginTop: 20}}
+                    onPress={handleNew}
                 />
             </Content>
         </Container>
