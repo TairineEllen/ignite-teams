eacef7a3d33bccd795f24296b56b191ec951b202
TairineEllen <tairine.ellen@gmail.com>
2025-04-24 11:46:31 -0300

    add routes

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
 import theme from './src/theme';
-import { Groups } from '@screens/Groups';
 import { Loading } from '@components/Loading';
-import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+import { Routes } from 'src/routes';
 
 
 export default function App() {
@@ -19,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
